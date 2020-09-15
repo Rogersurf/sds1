7bef57dc47192c4830106eb288709be3985ee9a9
Roger Braun <rogeriobflima@gmail.com>
2020-09-15 10:49:06 +0200

    POST records

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/dto/RecordInsertDTO.java
@@ -0,0 +1,41 @@
+package com.devsuperior.dspesquisa.dto;
+
+import java.io.Serializable;
+
+public class RecordInsertDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private int age;
+	private Long gameId;
+	
+	public RecordInsertDTO () {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Long getGameId() {
+		return gameId;
+	}
+
+	public void setGameID(Long gameId) {
+		this.gameId = gameId;
+	}
+	
+	
+}

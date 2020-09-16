fff5f844a1cc7a072a6eb080a193881fb717ed11
Roger Braun <rogeriobflima@gmail.com>
2020-09-16 15:52:39 +0200

    Navigation with routes created

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Home from './pages/Home';
-// Na linha acima era pra ser: './components/Header'
-
+import Routes from './Routes';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <Routes />
   );
 }
 

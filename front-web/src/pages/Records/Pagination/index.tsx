2a0d25154bb1a26137785b26016a78c70edcbbd4
Roger Braun <rogeriobflima@gmail.com>
2020-09-17 10:40:03 +0200

    Graphic page added

--- front-web/src/pages/Records/Pagination/index.tsx
+++ front-web/src/pages/Records/Pagination/index.tsx
@@ -12,7 +12,8 @@ const Pagination = ({ totalPages = 0, goToPage, activePage }: Props) => {
     return (
         <div className="pagination-container">
             {paginationItems.map(item => (
-                <button 
+                <button
+                key={item} 
                 className={`pagination-item ${activePage === item ? 'active' : 'inactive'}`}
                 onClick={() => goToPage(item)}
                 >
